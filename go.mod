aae4cdb3b025dbbc08c26118d87634af5e9f73c7
Xuanwo <github@xuanwo.io>
2021-01-12 14:45:49 +0800

    storage: Implement proposal Unify List Operation
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aos-dev/go-integration-test/v2
 go 1.14
 
 require (
-	github.com/aos-dev/go-storage/v2 v2.0.0-20201030030412-57f6d503c7c7
-	github.com/google/uuid v1.1.1
+	github.com/aos-dev/go-storage/v2 v2.0.1-0.20210112061652-0b7e2ab57b88
+	github.com/google/uuid v1.1.4
 	github.com/smartystreets/goconvey v1.6.4
 )
--- storager.go
+++ storager.go
@@ -196,5 +196,59 @@ func TestStorager(t *testing.T, store types.Storager) {
 				So(o, ShouldBeNil)
 			})
 		})
+
+		Convey("When List an empty dir", func() {
+			it, err := store.List("")
+
+			Convey("The error should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("The iterator should not be nil", func() {
+				So(it, ShouldNotBeIn)
+			})
+
+			o, err := it.Next()
+
+			Convey("The next should be done", func() {
+				So(err, ShouldBeError, types.IterateDone)
+			})
+			Convey("The object should be nil", func() {
+				So(o, ShouldBeNil)
+			})
+		})
+
+		Convey("When List a dir within files", func() {
+			size := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
+			r := io.LimitReader(randbytes.NewRand(), size)
+			path := uuid.New().String()
+			_, err := store.Write(path, r, ps.WithSize(size))
+			if err != nil {
+				t.Error(err)
+			}
+			defer func() {
+				err := store.Delete(path)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			it, err := store.List("")
+			Convey("The error should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("The iterator should not be nil", func() {
+				So(it, ShouldNotBeIn)
+			})
+
+			o, err := it.Next()
+			Convey("The name and size should be match", func() {
+				So(o, ShouldNotBeNil)
+				So(o.Name, ShouldEqual, path)
+
+				osize, ok := o.GetSize()
+				So(ok, ShouldBeTrue)
+				So(osize, ShouldEqual, size)
+			})
+		})
 	})
 }
