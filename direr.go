10cc763b6bfafb219cd26be38d7863706cf0223c
JinnyYi <jinnyyi@yunify.com>
2021-06-09 11:27:30 +0800

    Implement GSP-93 Add ObjectMode Pair

--- direr.go
+++ direr.go
@@ -1,11 +1,14 @@
 package tests
 
 import (
+	"errors"
+	"github.com/beyondstorage/go-storage/v4/services"
 	"testing"
 
 	"github.com/google/uuid"
 	. "github.com/smartystreets/goconvey/convey"
 
+	"github.com/beyondstorage/go-storage/v4/pairs"
 	"github.com/beyondstorage/go-storage/v4/types"
 )
 
@@ -16,18 +19,80 @@ func TestDirer(t *testing.T, store types.Storager) {
 			So(ok, ShouldBeTrue)
 		})
 
-		Convey("When CreateDir", func() {
+		Convey("When CreateDir with path not end with `/`", func() {
 			d, _ := store.(types.Direr)
 
 			path := uuid.New().String()
 			o, err := d.CreateDir(path)
 
 			defer func() {
-				err := store.Delete(path)
+				err := store.Delete(path, pairs.WithObjectMode(types.ModeDir))
 				if err != nil {
 					t.Error(err)
 				}
 			}()
+
+			Convey("The first returning error should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			o, err = d.CreateDir(path)
+			Convey("The second returning error also should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("The Object Mode should be dir", func() {
+				// Dir object's mode must be Dir.
+				So(o.Mode.IsDir(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When CreateDir with path end with `/`", func() {
+			d, _ := store.(types.Direr)
+
+			path := uuid.New().String()
+			path += "/"
+			_, err := d.CreateDir(path)
+
+			Convey("The error should be ErrRestrictionDissatisfied", func() {
+				So(errors.Is(err, services.ErrRestrictionDissatisfied), ShouldBeTrue)
+			})
+		})
+
+		Convey("When Create with ModeDir", func() {
+			path := uuid.New().String()
+			o := store.Create(path, pairs.WithObjectMode(types.ModeDir))
+
+			defer func() {
+				err := store.Delete(path, pairs.WithObjectMode(types.ModeDir))
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			Convey("The Object Mode should be dir", func() {
+				// Dir object's mode must be Dir.
+				So(o.Mode.IsDir(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When Stat with ModeDir", func() {
+			d, _ := store.(types.Direr)
+
+			path := uuid.New().String()
+			_, err := d.CreateDir(path)
+			if err != nil {
+				t.Error(err)
+			}
+
+			defer func() {
+				err := store.Delete(path, pairs.WithObjectMode(types.ModeDir))
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			o, err := store.Stat(path, pairs.WithObjectMode(types.ModeDir))
 
 			Convey("The error should be nil", func() {
 				So(err, ShouldBeNil)
@@ -36,6 +101,26 @@ func TestDirer(t *testing.T, store types.Storager) {
 			Convey("The Object Mode should be dir", func() {
 				// Dir object's mode must be Dir.
 				So(o.Mode.IsDir(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When Delete with ModeDir", func() {
+			d, _ := store.(types.Direr)
+
+			path := uuid.New().String()
+			_, err := d.CreateDir(path)
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = store.Delete(path, pairs.WithObjectMode(types.ModeDir))
+			Convey("The first returning error should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			err = store.Delete(path, pairs.WithObjectMode(types.ModeDir))
+			Convey("The second returning error also should be nil", func() {
+				So(err, ShouldBeNil)
 			})
 		})
 	})
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/beyondstorage/go-integration-test/v4
 go 1.15
 
 require (
-	github.com/beyondstorage/go-storage/v4 v4.0.1-0.20210530044854-1c928ddbe52d
+	github.com/beyondstorage/go-storage/v4 v4.1.0
 	github.com/google/uuid v1.2.0
 	github.com/smartystreets/goconvey v1.6.4
 )
