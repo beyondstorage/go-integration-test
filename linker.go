ada681e1e827bbab251a813eb94f5b21ae88c5d4
abyss-w <mad.hatter@foxmail.com>
2021-07-23 19:16:24 +0800

    test: Modify some cases in linker integration test

--- linker.go
+++ linker.go
@@ -2,7 +2,7 @@ package tests
 
 import (
 	"bytes"
-	"errors"
+	"crypto/md5"
 	"io"
 	"io/ioutil"
 	"math/rand"
@@ -12,7 +12,6 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 
 	"github.com/beyondstorage/go-storage/v4/pkg/randbytes"
-	"github.com/beyondstorage/go-storage/v4/services"
 	"github.com/beyondstorage/go-storage/v4/types"
 )
 
@@ -23,107 +22,124 @@ func TestLinker(t *testing.T, store types.Storager) {
 
 		Convey("When create a link object", func() {
 			size := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
+			r := io.LimitReader(randbytes.NewRand(), size)
+			target := uuid.New().String()
+
+			_, err := store.Write(target, r, size)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			defer func() {
+				err = store.Delete(target)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			path := uuid.New().String()
+			o, err := l.CreateLink(path, target)
+
+			defer func() {
+				err = store.Delete(path)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			Convey("The error should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("The object mode should be link", func() {
+				// Link object's mode must be link.
+				So(o.Mode.IsLink(), ShouldBeTrue)
+			})
+
+			Convey("The linkTarget of the object must be the same as the target", func() {
+				// The linkTarget must be the same as the target.
+				linkTarget, ok := o.GetLinkTarget()
+
+				So(ok, ShouldBeTrue)
+				So(linkTarget, ShouldEqual, target)
+			})
+
+			Convey("Stat should get path object without error", func() {
+				obj, err := store.Stat(path)
+
+				Convey("The error should be nil", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("The object mode should be link", func() {
+					// Link object's mode must be link.
+					So(obj.Mode.IsLink(), ShouldBeTrue)
+				})
+
+				Convey("The linkTarget of the object must be the same as the target", func() {
+					// The linkTarget must be the same as the target.
+					linkTarget, ok := obj.GetLinkTarget()
+
+					So(ok, ShouldBeTrue)
+					So(linkTarget, ShouldEqual, target)
+				})
+			})
+		})
+
+		Convey("When create a link object from a not existing target", func() {
+			target := uuid.New().String()
+
+			path := uuid.New().String()
+			o, err := l.CreateLink(path, target)
+
+			defer func() {
+				err = store.Delete(path)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			Convey("The error should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("The object mode should be link", func() {
+				// Link object's mode must be link.
+				So(o.Mode.IsLink(), ShouldBeTrue)
+			})
+
+			Convey("The linkTarget of the object must be the same as the target", func() {
+				linkTarget, ok := o.GetLinkTarget()
+
+				So(ok, ShouldBeTrue)
+				So(linkTarget, ShouldEqual, target)
+			})
+
+			Convey("Stat should get path object without error", func() {
+				obj, err := store.Stat(path)
+
+				Convey("The error should be nil", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("The object mode should be link", func() {
+					// Link object's mode must be link.
+					So(obj.Mode.IsLink(), ShouldBeTrue)
+				})
+
+				Convey("The linkTarget of the object must be the same as the target", func() {
+					// The linkTarget must be the same as the target.
+					linkTarget, ok := obj.GetLinkTarget()
+
+					So(ok, ShouldBeTrue)
+					So(linkTarget, ShouldEqual, target)
+				})
+			})
+
+			size := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
 			content, _ := ioutil.ReadAll(io.LimitReader(randbytes.NewRand(), size))
-			path := uuid.New().String()
 
-			_, err := store.Write(path, bytes.NewReader(content), size)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			defer func() {
-				err = store.Delete(path)
-				if err != nil {
-					t.Error(err)
-				}
-			}()
-
-			target := uuid.New().String()
-			o, err := l.CreateLink(path, target)
-
-			defer func() {
-				err = store.Delete(target)
-				if err != nil {
-					t.Error(err)
-				}
-			}()
-
-			Convey("The error should be nil", func() {
-				So(err, ShouldBeNil)
-			})
-
-			Convey("The object must should be link", func() {
-				// Link object's mode must be link.
-				So(o.Mode.IsLink(), ShouldBeTrue)
-				// Link object's mode should not be read.
-				So(o.Mode.IsRead(), ShouldBeFalse)
-			})
-
-			Convey("The linkTarget of the object must be the same as the target", func() {
-				// The linkTarget must be the same as the target.
-				linkTarget, ok := o.GetLinkTarget()
-
-				So(ok, ShouldBeTrue)
-				So(linkTarget, ShouldEqual, target)
-			})
-		})
-
-		Convey("When create a link object from a not existing path", func() {
-			path := uuid.New().String()
-
-			target := uuid.New().String()
-
-			Convey("Stat should get path object not exist", func() {
-				// Path does not exist so there is no object on path
-				_, err := store.Stat(path)
-
-				Convey("The err should be ErrObjectNotExist", func() {
-					So(errors.Is(err, services.ErrObjectNotExist), ShouldBeTrue)
-				})
-			})
-
-			o, err := l.CreateLink(path, target)
-
-			Convey("The error should be ErrObjectNotExist", func() {
-				So(errors.Is(err, services.ErrObjectNotExist), ShouldBeTrue)
-			})
-
-			Convey("The object should be nil", func() {
-				So(o, ShouldBeNil)
-			})
-
-			Convey("Stat should get target object mot exist", func() {
-				// Path does not exist so no object will be created on target
-				_, err = store.Stat(target)
-
-				Convey("The err should be ErrObjectNotExist", func() {
-					So(errors.Is(err, services.ErrObjectNotExist), ShouldBeTrue)
-				})
-			})
-		})
-
-		Convey("When CreateLink to an existing target", func() {
-			pathSize := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
-			pathContent, _ := ioutil.ReadAll(io.LimitReader(randbytes.NewRand(), pathSize))
-			path := uuid.New().String()
-
-			_, err := store.Write(path, bytes.NewReader(pathContent), pathSize)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			defer func() {
-				err = store.Delete(path)
-				if err != nil {
-					t.Error(err)
-				}
-			}()
-
-			targetSize := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
-			targetContent, _ := ioutil.ReadAll(io.LimitReader(randbytes.NewRand(), targetSize))
-			target := uuid.New().String()
-
-			_, err = store.Write(target, bytes.NewReader(targetContent), targetSize)
+			_, err = store.Write(target, bytes.NewReader(content), size)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -135,46 +151,101 @@ func TestLinker(t *testing.T, store types.Storager) {
 				}
 			}()
 
-			o, err := l.CreateLink(path, target)
+			Convey("Read should get path object data without error", func() {
+				var buf bytes.Buffer
+				n, err := store.Read(path, &buf)
 
-			Convey("The error should be nil", func() {
+				Convey("The error should be nil", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("The content should be match", func() {
+					So(n, ShouldNotBeNil)
+					So(n, ShouldEqual, size)
+					So(md5.Sum(buf.Bytes()), ShouldResemble, md5.Sum(content))
+				})
+			})
+		})
+
+		Convey("When CreateLink to an existing path", func() {
+			firstSize := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
+			r := io.LimitReader(randbytes.NewRand(), firstSize)
+			firstTarget := uuid.New().String()
+
+			_, err := store.Write(firstTarget, r, firstSize)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			defer func() {
+				err = store.Delete(firstTarget)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			path := uuid.New().String()
+			o, err := l.CreateLink(path, firstTarget)
+
+			defer func() {
+				err = store.Delete(path)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			Convey("The first returned error should be nil", func() {
 				So(err, ShouldBeNil)
 			})
 
-			Convey("The object must should be link", func() {
-				// Link object's mode must be link.
-				So(o.Mode.IsLink(), ShouldBeTrue)
-				// Link object's mode should not be read.
-				So(o.Mode.IsRead(), ShouldBeFalse)
+			secondSize := rand.Int63n(4 * 1024 * 1024) // Max file size is 4MB
+			secondContent, _ := ioutil.ReadAll(io.LimitReader(randbytes.NewRand(), secondSize))
+			secondTarget := uuid.New().String()
+
+			_, err = store.Write(secondTarget, bytes.NewReader(secondContent), secondSize)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			defer func() {
+				err = store.Delete(secondTarget)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+
+			o, err = l.CreateLink(path, secondTarget)
+
+			Convey("The second returned error should also be nil", func() {
+				So(err, ShouldBeNil)
 			})
 
-			Convey("The linkTarget of the object must be the same as the target", func() {
-				// The linkTarget must be the same as the target.
+			Convey("The object mode should be link", func() {
+				// Link object's mode must be link.
+				So(o.Mode.IsLink(), ShouldBeTrue)
+			})
+
+			Convey("The linkTarget of the object must be the same as the secondTarget", func() {
+				// The linkTarget must be the same as the secondTarget.
 				linkTarget, ok := o.GetLinkTarget()
 
 				So(ok, ShouldBeTrue)
-				So(linkTarget, ShouldEqual, target)
+				So(linkTarget, ShouldEqual, secondTarget)
 			})
 
-			Convey("List should get target ObjectIterator", func() {
-				it, err := store.List(target)
+			Convey("Read should get path object data without error", func() {
+				var buf bytes.Buffer
+				n, err := store.Read(path, &buf)
 
 				Convey("The error should be nil", func() {
 					So(err, ShouldBeNil)
 				})
 
-				Convey("The iterator should not be nil", func() {
-					So(it, ShouldNotBeNil)
-				})
-
-				obj, err := it.Next()
-
-				Convey("The error should be nil", func() {
-					So(err, ShouldBeNil)
-				})
-
-				Convey("The object should not be nil", func() {
-					So(obj, ShouldNotBeNil)
+				Convey("The content should be match", func() {
+					// The content should match the secondTarget
+					So(n, ShouldNotBeNil)
+					So(n, ShouldEqual, secondSize)
+					So(md5.Sum(buf.Bytes()), ShouldResemble, md5.Sum(secondContent))
 				})
 			})
 		})
